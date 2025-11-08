82b0556f609f2f85ddbc225b17f2cdb7b150fe18
LaraMaia <laracarolinemaya@gmail.com>
2025-11-07 22:05:36 -0400

    Entidade

---
+++ src/StageFlowVag.Domain/Entities/Usuario.cs
@@ -0,0 +1,14 @@
+﻿using StageFlowVag.Domain.Base;
+using StageFlowVag.Domain.Enums;
+
+namespace StageFlowVag.Domain.Entities
+{
+    public class Usuario : BaseEntity
+    {
+        public string Matricula { get; set; }
+        public string Nome { get; set; } 
+        public string Senha { get; set; }
+        public string Email { get; set; }
+        public UsuarioEnum Role { get; set; }
+    }
+}
